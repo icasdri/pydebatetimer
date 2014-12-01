7c3c657f4bf92e83462a7564a69a78a750a4399f
icasdri <icasdri@gmail.com>
2014-12-01 16:02:43 -0500

    Added license header and copyright notice.

--- pydebatetimer/main.py
+++ pydebatetimer/main.py
@@ -1,3 +1,17 @@
+# Copyright 2014 icasdri
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from gi.repository import GObject, Gtk, Gio, Pango
 from functools import partial
 
@@ -78,7 +92,10 @@ class TimerButton(Gtk.ToggleButton):
         if self.get_active():
             if self.countdown:
                 if self.seconds == 0:
-                    if self.minutes != 0:
+                    if self.minutes == 0:
+                        # You're outta time!
+                        self.set_active(False)
+                    else:
                         self.seconds = 59
                         self.minutes -= 1
                         self._update_text()
